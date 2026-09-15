1b7ce6452b35fc9d97657b69e355f537494c38bd
cleones1 <servcom31@gmail.com>
2026-09-15 14:21:54 -0600

    Load dues breakdown and receipt tools

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 from .core import app
 from .admin_tools import router as admin_tools_router
+from .dues_tools import router as dues_tools_router
 
 app.include_router(admin_tools_router)
+app.include_router(dues_tools_router)
